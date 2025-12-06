a921162a374f3fa0b1f4e9c34e379fbb77cbaa86
Tanmay-Anand <tanmayanand2002@gmail.com>
2025-12-07 01:22:55 +0530

    Initial commit: SnapList monorepo with backend and frontend

---
+++ snaplist-backend/src/main/java/com/snaplist/SnaplistBackendApplication.java
@@ -0,0 +1,13 @@
+package com.snaplist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnaplistBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnaplistBackendApplication.class, args);
+	}
+
+}
